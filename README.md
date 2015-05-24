e8650bb9f8e15c5aff85ffbc158e6fddf1757484
kadoyama <kadoyama@team-lab.com>
2015-05-24 21:28:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+
+# Installation
+
+### Zabbix server
+
+```
+cd /usr/lib/zabbix/alertscripts
+wget https://raw.githubusercontent.com/bageljp/zabbix-slack/master/slack.sh
+chmod a+x slack.sh
+vi slack.sh
+```
+
+<table>
+  <tr>
+    <td><tt>slack_url</tt></td>
+	<td>slack integration url.</td>
+  </tr>
+  <tr>
+    <td><tt>slack_username</tt></td>
+    <td>slack username.</td>
+  </tr>
+  <tr>
+    <td><tt>emoji</tt></td>
+    <td>slack emoji.</td>
+  </tr>
+</table>
+
+### Zabbix Server settings
+
+* Administration MediaType
+<table>
+  <tr>
+    <td><tt>Name</tt></td>
+	<td>Slack</td>
+  </tr>
+  <tr>
+    <td><tt>Type</tt></td>
+    <td>script</td>
+  </tr>
+  <tr>
+    <td><tt>Script name</tt></td>
+    <td>slack.sh</td>
+  </tr>
+</table>
+
+* Action settings ``default message``
+```
+HOST: {HOST.NAME}
+TRIGGER_NAME: {TRIGGER.NAME}
+TRIGGER_STATUS: {TRIGGER.STATUS}
+TRIGGER_SEVERITY: {TRIGGER.SEVERITY}
+DATETIME: {DATE} / {TIME}
+ITEM_NAME: {ITEM.NAME1}
+ITEM_KEY: {ITEM.KEY1}
+ITEM_VALUE: {ITEM.VALUE1}
+EVENT_ID: {EVENT.ID}
+TRIGGER_URL: {TRIGGER.URL}
+```
+
---
+++ slack.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# config
+slack_url='https://hooks.slack.com/services/XXXXX'
+slack_username='Zabbix'
+channel="$1"
+title="$2"
+params="$3"
+emoji=':zabbix:'
+timeout="5"
+cmd_curl="/usr/bin/curl"
+
+# set params
+host="`echo \"${params}\" | grep 'HOST: ' | awk -F'HOST: ' '{print $2}' | sed -e 's///g'`"
+trigger_name="`echo \"${params}\" | grep 'TRIGGER_NAME: ' | awk -F'TRIGGER_NAME: ' '{print $2}' | sed -e 's///g'`"
+trigger_status="`echo \"${params}\" | grep 'TRIGGER_STATUS: ' | awk -F'TRIGGER_STATUS: ' '{print $2}' | sed -e 's///g'`"
+trigger_severity="`echo \"${params}\" | grep 'TRIGGER_SEVERITY: ' | awk -F'TRIGGER_SEVERITY: ' '{print $2}' | sed -e 's///g'`"
+trigger_url="`echo \"${params}\" | grep 'TRIGGER_URL: ' | awk -F'TRIGGER_URL: ' '{print $2}' | sed -e 's///g'`"
+datetime="`echo \"${params}\" | grep 'DATETIME: ' | awk -F'DATETIME: ' '{print $2}' | sed -e 's///g'`"
+item_value="`echo \"${params}\" | grep 'ITEM_VALUE: ' | awk -F'ITEM_VALUE: ' '{print $2}' | sed -e 's///g'`"
+event_id="`echo \"${params}\" | grep 'EVENT_ID: ' | awk -F'EVENT_ID: ' '{print $2}' | sed -e 's///g'`"
+
+# set color
+if [ "${trigger_status}" == 'OK' ]; then
+  case "${trigger_severity}" in
+    'Information')
+      color="#439FE0"
+      ;;
+    *)
+      color="good"
+      ;;
+  esac
+elif [ "${trigger_status}" == 'PROBLEM' ]; then
+  case "${trigger_severity}" in
+    'Information')
+      color="#439FE0"
+      ;;
+    'Warning')
+      color="warning"
+      ;;
+    *)
+      color="danger"
+      ;;
+  esac
+else
+  color="#808080"
+fi
+
+# set payload
+payload="payload={
+  \"channel\": \"${channel}\",
+  \"username\": \"${slack_username}\",
+  \"icon_emoji\": \"${emoji}\",
+  \"attachments\": [
+    {
+      \"fallback\": \"Date / Time: ${datetime} - ${title}\",
+      \"title\": \"${title}\",
+      \"title_link\": \"${trigger_url}\",
+      \"color\": \"${color}\",
+      \"fields\": [
+        {
+            \"title\": \"Date / Time\",
+            \"value\": \"${datetime}\",
+            \"short\": true
+        },
+        {
+            \"title\": \"Status\",
+            \"value\": \"${trigger_status}\",
+            \"short\": true
+        },
+        {
+            \"title\": \"Host\",
+            \"value\": \"${host}\",
+            \"short\": true
+        },
+        {
+            \"title\": \"Trigger\",
+            \"value\": \"${trigger_name}: ${item_value}\",
+            \"short\": true
+        }
+      ]
+    }
+  ]
+}"
+
+# send to slack
+${cmd_curl} -m ${timeout} --data-urlencode "${payload}" "${slack_url}"
+
